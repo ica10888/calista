950807862b2cb28fd0044a4d94868f869ca2d12d
ica10888 <ica10888@gmail.com>
2020-10-27 22:01:08 +0800

    first commit

---
+++ src/main/java/com/nio/config/NioConfig.java
@@ -0,0 +1,12 @@
+package com.nio.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+@ConfigurationProperties("nio.config")
+public class NioConfig {
+
+
+}
---
+++ src/main/java/com/nio/service/ReceiverService.java
@@ -0,0 +1,8 @@
+package com.nio.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ReceiverService {
+
+}
